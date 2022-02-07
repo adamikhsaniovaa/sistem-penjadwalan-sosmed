70eeecfc886e8672a5563c840131dcef7c8159d8
adamikhsaniovaa <98997990+adamikhsaniovaa@users.noreply.github.com>
2022-02-07 23:16:30 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -15,15 +15,18 @@ if(isset($_GET['Id'])){
 		//query ke database DELETE untuk menghapus data dengan kondisi id=$id
 		$del = mysqli_query($koneksi, "DELETE FROM konten WHERE ID='$ID'") or die(mysqli_error($koneksi));
 		if($del){
-			echo '<script>alert("Berhasil menghapus data."); document.location="index.php?page=tampil_mhs";</script>';
+			echo '<script>alert("Berhasil menghapus data."); 
+			document.location="tampil.php";</script>';
 		}else{
-			echo '<script>alert("Gagal menghapus data."); document.location="index.php?page=tampil_mhs";</script>';
+			echo '<script>alert("Gagal menghapus data.");
+			 document.location="tampil.php";</script>';
 		}
 	}else{
-		echo '<script>alert("ID tidak ditemukan di database."); document.location="index.php?page=tampil_mhs";</script>';
+		echo '<script>alert("ID tidak ditemukan di database."); document.location="tampil.php";</script>';
 	}
 }else{
-	echo '<script>alert("ID tidak ditemukan di database."); document.location="index.php?page=tampil_mhs";</script>';
+	echo '<script>alert("ID tidak ditemukan di database."); 
+			document.location="tampil.php";</script>';
 }
 
 ?>
